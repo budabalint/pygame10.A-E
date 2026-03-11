f0da3a7a0c16f1373326519bd815467ba313ce6e
budabalint <buda.balint@students.jedlik.eu>
2026-03-11 09:38:02 +0100

    rács színezés tesztelve

--- game.py
+++ game.py
@@ -6,12 +6,15 @@ from grid import Grid
 class Game:
     def __init__(self) -> None:
         pygame.init()
-        self.ablak: pygame.Surface = pygame.display.set_mode((1200, 600))
+        self.ablak: pygame.Surface = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Tetris")
         self.sötétkék: tuple[int ,int ,int] = (50, 50, 130)
 
         self.óra = pygame.time.Clock()  # a játék sebességét adja meg
         self.rács: Grid = Grid()
+        self.rács.rács[0][0] = 1
+        self.rács.rács[0][5] = 3
+        self.rács.rács[4][6] = 2
         self.rács.rács_kiíratása_consolra()
 
     def game_loop(self) -> None:
